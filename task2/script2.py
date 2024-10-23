3f4ba0302b146ac8b9e6adb5df3037430d0d90f1
De1eF <jatfun58@gmail.com>
2024-10-23 14:12:06 +0300

    Additional operation for scirpt2

--- task2/script2.py
+++ task2/script2.py
@@ -1,12 +1,32 @@
 import json
 
 elementsArr = ()
-requestedElement = input("Enter element name: ")
+selectedOperation = input("Select operation, (m) - find mass by name, (n) - find data by first letter of name: ")
 fileName = "task2/elements.json"
+
+def selectOperation(elDic):
+    if (selectedOperation == "m"):
+        findByMassFromDic(elDic)
+    elif (selectedOperation == "n"):
+        getDataOfElementsStartingWith(elDic)
+    else:
+        print("Unknown action")
+        
+def getDataOfElementsStartingWith(elDic):
+    symbol = input("Letter: ")
+    elDicc = dict(elDic)
+    for el in elDicc.items():
+        if str(el[0][0]).lower() == symbol.lower():
+            print("Mass of ", el[0], " is: ", el[1])
+
+def findByMassFromDic(elDic):
+    requestedElement = input("Enter element name: ")
+    if (not requestedElement in elDic):
+        print("No ", requestedElement, " in database")
+        exit()
+    print("Mass of ", requestedElement, " is: ", elDic[requestedElement])
+        
 with open(fileName, "r") as elements:
     elementsArr = json.load(elements)["elements"]
     elementsDict = {el["name"]: el["mass"] for el in elementsArr}
-    if (not requestedElement in elementsDict):
-        print("No ", requestedElement, " in database")
-        exit()
-    print("Mass of ", requestedElement, " is: ", elementsDict[requestedElement])
+    selectOperation(elementsDict)
